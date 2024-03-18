a9d385677966200893cd569a9c4779760a254b23
Your Name <you@example.com>
2024-03-17 22:15:07 -0500

    adding dict of list of dicts

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Module: API script for retrieving
+"""
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    user_ext = '/users/{}'.format(sys.argv[1])
+    todo_ext = '/todos'
+    file_name = "{}.json".format(sys.argv[1])
+
+    employee_response = requests.get(base_url+user_ext)
+    employee = employee_response.json()
+    todo_response = requests.get(base_url+user_ext+todo_ext)
+    todos = todo_response.json()
+
+    json_key = "{}".format(employee['id'])
+    employee_json = {json_key: []}
+    for todo in todos:
+        task_dict = {
+            "task": todo['title'],
+            "completed": todo['completed'],
+            "username": employee['username']}
+        employee_json[json_key].append(task_dict)
+
+        with open(file_name, 'w') as file:
+            file.write(json.dumps(employee_json))
---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+Module defining an API retrieval script
+"""
+
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    user_ext = '/users/'
+    todo_ext = '/todos'
+
+    employees_dict = {}
+    employee_response = requests.get(base_url+user_ext)
+    employees = employee_response.json()
+
+    for employee in employees:
+        json_key = "{}".format(employee['id'])
+        employee_json = {json_key: []}
+        todo_response = requests.get(base_url+user_ext+json_key+todo_ext)
+        todos = todo_response.json()
+        for todo in todos:
+            task_dict = {
+                "username": employee['username'],
+                "task": todo['title'],
+                "completed": todo['completed']}
+            employee_json[json_key].append(task_dict)
+        employees_dict.update(employee_json)
+
+    with open('todo_all_employees.json', 'w') as file:
+        file.write(json.dumps(employees_dict))

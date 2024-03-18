be6a614955aff6ef266f8d78ae81a16d467aaf8a
Your Name <you@example.com>
2024-03-17 20:47:31 -0500

    Fixed Pylance spacing/closing+Typos

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     todo_ext = '/todos'
 
     employee_response = requests.get(base_url+user_ext)
-    emlpoyee = employee_response.json()
+    employee = employee_response.json()
     todo_response = requests.get(base_url+user_ext+todo_ext)
     todos = todo_response.json()
     completed = 0
@@ -25,9 +25,9 @@ if __name__ == '__main__':
         if todo['completed'] is True:
             completed += 1
 
-    print("Employee {0} is done with tasks"({1}/{2}):".format"
-          (employee['name'], completed, total))
-    
+    print("Employee {0} is done with tasks({1}/{2}):".format(
+        employee['name'], completed, total
+        ))
     for todo in todos:
         if todo['completed'] is True:
             print("\t {}".format(todo['title']))
